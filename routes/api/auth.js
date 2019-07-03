695d7b200db41e3fda1fb7feeb3aed83c8208d81
xingchenzhao <xiz168@pitt.edu>
2019-07-03 00:38:19 -0400

    Created the Profile Model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const USer = require('../../models/User');
+const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
