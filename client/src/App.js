bac1acb8976eaa86cec6e0e73f3ee87deb930a2d
xingchenzhao <xiz168@pitt.edu>
2019-07-24 02:39:53 -0400

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -69,42 +69,40 @@ const App = () => {
         <Router>
           <Fragment>
             <Navbar />
-            <section>
-              <Alert />
-              <Switch>
-                <Route exact path='/' component={Landing} />
-                <section className='container'>
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
-                  <Route exact path='/profiles' component={Profiles} />
-                  <Route exact path='/profile/:id' component={Profile} />
+            <Alert />
+            <Switch>
+              <Route exact path='/' component={Landing} />
+              <section className='container'>
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/profiles' component={Profiles} />
+                <Route exact path='/profile/:id' component={Profile} />
 
-                  <PrivateRoute exact path='/dashboard' component={Dashboard} />
-                  <PrivateRoute
-                    exact
-                    path='/create-profile'
-                    component={CreateProfile}
-                  />
-                  <PrivateRoute
-                    exact
-                    path='/edit-profile'
-                    component={EditProfile}
-                  />
-                  <PrivateRoute
-                    exact
-                    path='/add-experience'
-                    component={AddExperience}
-                  />
-                  <PrivateRoute
-                    exact
-                    path='/add-education'
-                    component={AddEducation}
-                  />
-                  <PrivateRoute exact path='/posts' component={Posts} />
-                  <PrivateRoute exact path='/posts/:id' component={Post} />
-                </section>
-              </Switch>
-            </section>
+                <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                <PrivateRoute
+                  exact
+                  path='/create-profile'
+                  component={CreateProfile}
+                />
+                <PrivateRoute
+                  exact
+                  path='/edit-profile'
+                  component={EditProfile}
+                />
+                <PrivateRoute
+                  exact
+                  path='/add-experience'
+                  component={AddExperience}
+                />
+                <PrivateRoute
+                  exact
+                  path='/add-education'
+                  component={AddEducation}
+                />
+                <PrivateRoute exact path='/posts' component={Posts} />
+                <PrivateRoute exact path='/posts/:id' component={Post} />
+              </section>
+            </Switch>
           </Fragment>
         </Router>
       </Provider>
